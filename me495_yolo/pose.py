0b35a1a60bf166d3eb70e36f74139dee44147b7a
Matthew Elwin <elwin@northwestern.edu>
2024-12-04 21:44:12 -0600

    Added method to change model

--- me495_yolo/pose.py
+++ me495_yolo/pose.py
@@ -6,21 +6,39 @@ from cv_bridge import CvBridge
 
 
 class YoloNode(Node):
-    """ Use Yolo to identify scene objects """
+    """
+    Use Yolo to identify scene objects
 
+    Subscribes
+    ----------
+    image (sensor_msgs/msg/Image) - The input image
+
+    Publishes
+    ---------
+    new_image (sensor_msgs/msg/Image) - The image with the detections
+
+    Parameters
+    model (string) - The Yolo model to use: see docs.ultralytics.org for available values. Default is yolo11n.pt
+    """
     def __init__(self):
         super().__init__("pose")
         self.bridge = CvBridge()
-        self.model = YOLO("yolo11n-pose.pt")
+        self.declare_parameter("model",
+                               value="yolo11n.pt")
+        self.model = YOLO(self.get_parameter("model").get_parameter_value().string_value)
         self.create_subscription(Image, 'image', self.yolo_callback, 10)
         self.pub = self.create_publisher(Image, 'new_image', 10)
 
     def yolo_callback(self, image):
-        """Draw a circle on the subscribed image and republish it to new_image."""
+        """Identify all the objects in the scene"""
+        # Convert to OpenCV
         cv_image = self.bridge.imgmsg_to_cv2(image, desired_encoding='bgr8')
+        # Run the model
         results = self.model(cv_image)
+        # Get the result and draw it on an OpenCV image
         frame = results[0].plot()
         new_msg = self.bridge.cv2_to_imgmsg(frame, encoding='bgr8')
+        # publish
         self.pub.publish(new_msg)
 
 def main():
